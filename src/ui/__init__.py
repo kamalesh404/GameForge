36d00a6a422ecc6f2111c20d446581769c4bcdfa
Kamalesh <kamalesh2017d@gmail.com>
2026-08-24 21:29:19 +0530

    feat: GameForge game engine - ECS, renderer, physics, audio, networking

---
+++ src/ui/__init__.py
@@ -0,0 +1,19 @@
+"""UI subsystem: retained-mode widgets and the routing manager."""
+
+from src.ui.button import Button, ButtonState
+from src.ui.label import Label, TextAlignment, TextStyle
+from src.ui.manager import UIManager
+from src.ui.panel import Panel
+from src.ui.widget import Rect, Widget
+
+__all__ = [
+    "Button",
+    "ButtonState",
+    "Label",
+    "TextAlignment",
+    "TextStyle",
+    "UIManager",
+    "Panel",
+    "Rect",
+    "Widget",
+]
